57a052257f138b075c8dc55844ae3fa7dc6f52c9
lauro-ladeira <lauro.floresta@gmail.com>
2020-02-02 20:13:25 -0200

    Componente App refatorado para haver comunicação com o componente filho

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+
+  onSearchSubimit(TOPterm) {
+    console.log(TOPterm);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubimit} />
+      </div>
+    );
+  }
+}
 
 export default App;
+
+// refatorando o app componente p classe pra ele passar uma prop pra children com uma função de callback,
