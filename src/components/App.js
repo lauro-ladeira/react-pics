51e31e57cedc490bf8e90cc9cca2870ea64dd05a
lauro-ladeira <lauro.floresta@gmail.com>
2020-02-02 21:26:29 -0200

    API request do unsplash feita usando a sintaxe async/await

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  async onSearchSubimit(term) {
+    const response = await axios.get(
+      "https://api.unsplash.com//search/photos/?client_id=1a63a80e7aa16aa0f99ffc13db8e743edb1f8f15be36f2ae9d2cea9687fee099",
+      {
+        params: { query: term }
+      }
+    );
 
-  onSearchSubimit(TOPterm) {
-    console.log(TOPterm);
+    console.log(response);
   }
 
   render() {
@@ -19,3 +26,4 @@ class App extends React.Component {
 export default App;
 
 // refatorando o app componente p classe pra ele passar uma prop pra children com uma função de callback,
+//fazendo api requests
