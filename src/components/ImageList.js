aa66856b9aad97180528899b75f13dd4c2469e80
lauro-ladeira <lauro.floresta@gmail.com>
2020-02-04 15:54:21 -0200

    Componente ImageCard implementado e adicionado sistema grid css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,17 @@
+import './ImageList.css';
 import React from "react";
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} />;
+    return (
+      <ImageCard image={image} key={image.id} />
+    );
   });
 
-  return <div>{images}</div>
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
+
+// usou-se o sistema grid css para exbir as imagens
